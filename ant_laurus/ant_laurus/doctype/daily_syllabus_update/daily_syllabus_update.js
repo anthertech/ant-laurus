7df6e53a64792f59ea501d47c9bd080cbe5bf718
Hopesonjames <hopesonjames2018@gmail.com>
2025-08-25 13:14:14 +0530

    chore:ant_laurus file cleaning

--- ant_laurus/ant_laurus/doctype/daily_syllabus_update/daily_syllabus_update.js
+++ ant_laurus/ant_laurus/doctype/daily_syllabus_update/daily_syllabus_update.js
@@ -1,36 +1,6 @@
 // Copyright (c) 2025, Hopeson and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Daily Syllabus Update", {
-// 	refresh(frm) {
-
-// 	},
-// });
-// frappe.ui.form.on('Daily Syllabus Update', {
-//     course: function(frm) {
-//         if (frm.doc.course) {
-//             // Call the server script to fetch topics based on the selected course
-//             frappe.call({
-//                 method: "ant_laurus.ant_laurus.doctype.daily_syllabus_update.get_course_topics",  // Path to your server method
-//                 args: {
-//                     course_name: frm.doc.course  // Pass the selected course
-//                 },
-//                 callback: function(response) {
-//                     if (response.message) {
-//                         // Set the topic field options based on the topics retrieved from the server
-//                         frm.set_df_property('topic', 'options', response.message);
-//                         frm.set_value('topic', null);  // Clear previously selected topic
-//                     }
-//                 }
-//             });
-//         } else {
-//             // Clear the topic field options if no course is selected
-//             frm.set_df_property('topic', 'options', []);
-//             frm.set_value('topic', null);
-//         }
-//     }
-// });
-
 frappe.ui.form.on('Daily Syllabus Update', {
     course: function(frm) {
         if (frm.doc.course) {
